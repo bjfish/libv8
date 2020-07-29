4a1c48cd4e494030d5da19b7d2e1a7c8382c13e6
Brandon Fish <brandon.j.fish@oracle.com>
2020-07-29 11:03:07 -0500

    Try v8_component_build

--- ext/libv8/builder.rb
+++ ext/libv8/builder.rb
@@ -19,7 +19,7 @@ module Libv8
          is_debug=#{debug_build? ? 'true' : 'false'}
          symbol_level=#{debug_build? ? '-1' : '0'}
          is_component_build=false
-         component=shared_library
+         v8_component_build=true
          v8_monolithic=true
          v8_use_external_startup_data=false
          target_cpu="#{libv8_arch}"
